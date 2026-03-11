128e5b4f7a21be04fc8bbf811a7812f4576d134a
LuiS-213 <luisangel.j280606@gmail.com>
2026-03-10 21:41:11 -0600

    Inicio del trabajo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Menu from './menu'
+import Body from './Body'
+
+
+function App() {
+  return(
+    <div>
+        <Menu/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Menu.jsx
@@ -0,0 +1,16 @@
+function Menu(){
+    return(
+        <div>
+            <ul>
+                <li>Inicio de Sesion</li>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Ventas</li>
+                <li>Reportes</li>
+                <li>Usuarios</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
