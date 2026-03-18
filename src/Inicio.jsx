f938f8a835ce7c800453ecb221380266160cb04e
jessi-cb6 <240491@utxicotepec.edu.mx>
2026-03-18 00:40:06 -0600

    jessi

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,8 +1,14 @@
-function Inicio(){
-    return(
-    <div>
-        <h1>Inicio</h1>
+import './inicioo.css'
+function Inicio() {
+  return (
+    <div className="inicio-container">
+      <h1>Bienvenido a FARMACLAROTI</h1>
+      <p>"MAS QUE MEDICINAS TE DAMOS CLARIDAD"</p>
+      <div className="cards">
+<img src="https://i.pinimg.com/736x/35/29/ea/3529ea82e96ee26b876a7fa761319f40.jpg" alt="" /> 
+      </div>
     </div>
-    )
+  );
 }
-export default Inicio
+
+export default Inicio;
--- src/InicioS.jsx
+++ src/InicioS.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './inicioS.css';
 
 const InicioS = ({ onLogin }) => {
   const [email, setEmail] = useState('');
@@ -6,29 +7,31 @@ const InicioS = ({ onLogin }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Validación
     if (email === "user@test.com" && password === "123") {
-      onLogin(); // onLogin() es lo que cambia la vista
+      onLogin();
     } else {
       alert("Credenciales incorrectas (user@test.com / 123)");
     }
   };
+
   return (
-    <div>
-      <h2>Bienvenido</h2>
-      <form onSubmit={handleSubmit} >
-        <input 
-          type="email" 
-          placeholder="Correo electrónico" 
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input 
-          type="password" 
-          placeholder="Contraseña" 
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Bienvenido</h2>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="email" 
+            placeholder="Correo electrónico" 
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input 
+            type="password" 
+            placeholder="Contraseña" 
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 };
